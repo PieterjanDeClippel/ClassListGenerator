0a90fea734f4f5d400dfeb0e8e57d67543cc8c52
PieterjanDeClippel <pieterjandeclippel@msn.com>
2025-09-15 20:21:36 +0200

    Add project files.

---
+++ ClassListGenerator/Models/ClassDeclaration.cs
@@ -0,0 +1,9 @@
+﻿using MintPlayer.ValueComparerGenerator.Attributes;
+
+namespace ClassListGenerator.Models;
+
+[AutoValueComparer]
+public partial class ClassDeclaration
+{
+    public string? Name { get; set; }
+}
